813937b181a94fdc8ce6bcc616346fec9d774d02
amills9 <89893713+amills9@users.noreply.github.com>
2021-10-13 14:48:34 +1100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FirstStepDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        void OnClick1(object sender, RoutedEventArgs e)
+        {
+            button1.Foreground = new SolidColorBrush(Colors.Blue);
+            MessageBox.Show("On Hover click event occurs.");
+        }
+        void OnClick2(object sender, RoutedEventArgs e)
+        {
+            button2.Foreground = new SolidColorBrush(Colors.Green);
+            MessageBox.Show("On Press click event occurs.");
+        }
+        void OnClick3(object sender, RoutedEventArgs e)
+        {
+            button1.Foreground = new SolidColorBrush(Colors.Green);
+            button2.Foreground = new SolidColorBrush(Colors.Blue);
+            MessageBox.Show("On Release click event occurs.");
+        }
+    }
+}
